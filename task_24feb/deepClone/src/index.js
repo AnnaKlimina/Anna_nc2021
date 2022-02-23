624bd8ca5a8891d82432d4a47e1633caec92d027
AnnaKlimina <55355491+AnnaKlimina@users.noreply.github.com>
2022-02-24 00:20:56 +0300

    Create index.js

---
+++ task_24feb/deepClone/src/index.js
@@ -0,0 +1,13 @@
+function copyObject(obj, copy = {}) {
+  if (typeof obj != "object") {
+    console.log("Неверный тип данных! Функция принимает только объекты!");
+    return {};
+  }
+  for (let key in obj) {
+    if (typeof obj[key] == "object") {
+      copy[key] = copyObject(obj[key]);
+    } else copy[key] = obj[key];
+  }
+  if (obj instanceof Array) return Array.from(Object.values(copy));
+  return obj.constructor(copy);
+}
