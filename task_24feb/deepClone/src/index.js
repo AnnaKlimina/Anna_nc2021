43d3f1d56bb7dc757e74130b6009345ec03ca6f5
AnnaKlimina <55355491+AnnaKlimina@users.noreply.github.com>
2022-02-28 21:56:11 +0300

    Update index.js

--- task_24feb/deepClone/src/index.js
+++ task_24feb/deepClone/src/index.js
@@ -1,13 +1,15 @@
-function copyObject(obj, copy = {}) {
+function deepClone(obj, copy = {}) {
   if (typeof obj != "object") {
     console.log("Неверный тип данных! Функция принимает только объекты!");
     return {};
   }
+
   for (let key in obj) {
     if (typeof obj[key] == "object") {
-      copy[key] = copyObject(obj[key]);
+      copy[key] = deepClone(obj[key]);
     } else copy[key] = obj[key];
   }
   if (obj instanceof Array) return Array.from(Object.values(copy));
+  if (obj instanceof Date) return new Date(obj);
   return new obj.constructor(copy);
 }
